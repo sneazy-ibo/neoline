f51b1aca604359b171fd11ea6d10616a6f967f68
sneazy-ibo <41344259+sneazy-ibo@users.noreply.github.com>
2025-08-12 02:24:20 +0200

    core: compile from devcore

--- neoline.user.js
+++ neoline.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Neoline Loader
-// @version      1.0
+// @version      2.0
 // @author       0x69626F - unreadable
-// @icon         https://raw.githubusercontent.com/sneazy-ibo/neoline/refs/heads/altair/assets/favicon.png
+// @icon         https://neolinelol.vercel.app/assets/favicon.png
 // @require      https://raw.githubusercontent.com/sneazy-ibo/neoline/refs/heads/altair/bootstrap.js
-// @description  Replaces the html of coolmath games powerline with a modified version
+// @description  Replaces the source code of coolmath games powerline with a modified version
+// @match        https://www.coolmathgames.com/*/0-powerline-io*
 // @match        https://www.coolmathgames.com/0-powerline-io*
 // @run-at       document-start
 // ==/UserScript==
